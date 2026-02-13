2e209b30e09ee5672f15f82daeaf4c33367d47ce
alina-geissler <alina.geissler@t-online.de>
2026-02-13 15:35:54 +0100

    Implement core game logic
    - Add variables to track mistakes and guessed letters
    - Add new function to display current game state (state of snowman and secret word with placeholders for letters not yet guessed)
    - For now, show initial state and ask user for a first guess

--- snowman.py
+++ snowman.py
@@ -1,5 +1,36 @@
 import random
 
+# Snowman ASCII Art stages
+STAGES = [
+     # Stage 0: Full snowman
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
+     """,
+     # Stage 1: Bottom part starts melting
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     """,
+     # Stage 2: Only the head remains
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     """,
+     # Stage 3: Snowman completely melted
+     """
+      ___  
+     /___\\ 
+     """
+ ]
+
+
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 
@@ -11,13 +42,25 @@ def get_random_word():
 
 def play_game():
     secret_word = get_random_word()
+    mistakes = 0
+    guessed_letters = []
     print("Welcome to Snowman Meltdown!")
-    print("Secret word selected: " + secret_word)  # for testing, later remove this line
-
-    # TODO: Build your game loop here.
-    # For now, simply prompt the user once:
-    guess = input("Guess a letter: ").lower()
+    # For now, show the initial game state
+    display_game_state(mistakes, secret_word, guessed_letters)
+    # For now, get the user input for one first guess
+    guess = input("\nGuess a letter: ").lower()
     print("You guessed:", guess)
+    guessed_letters.append(guess)
+
+
+def display_game_state(mistakes, secret_word, guessed_letters):
+    # Show the state of the snowman (depending on the number of mistakes)
+    print(STAGES[mistakes])
+    # Create the version of the secret word to be displayed
+    # (using placeholders for letters that have not yet been guessed)
+    guessed_so_far = [char if char in guessed_letters else "_" for char in secret_word]
+    print(f"(Secret word: {secret_word})")
+    print("Word: ", "  ".join(guessed_so_far))
 
 
 if __name__ == "__main__":
