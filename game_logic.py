cf21d92ae03b1da318b4e4a768a69baeb859d791
alina-geissler <alina.geissler@t-online.de>
2026-02-20 12:53:05 +0100

    refactor replay to prevent recursion issues and add docstrings

--- game_logic.py
+++ game_logic.py
@@ -8,18 +8,26 @@ colorama.init(autoreset=True)
 
 
 def get_random_word():
-    """Selects a random word from the list."""
+    """
+    Select a random word from the list.
+    :return: the word to guess
+    """
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
 
 def play_game():
+    """
+    Run the main loop of the game and coordinate the overall gameplay.
+    """
     secret_word = get_random_word()
     mistakes = 0
     guessed_letters = []
     print(colorama.Fore.MAGENTA + "\nGuess the secret word before the snowman melts away!")
     while True:
-        display_game_state(mistakes, secret_word, guessed_letters)
-        if mistakes < 3:
+        is_finished = display_game_state(mistakes, secret_word, guessed_letters)
+        if mistakes == 3 or is_finished:
+            break
+        elif mistakes < 3:
             guess = get_valid_guess(guessed_letters)
             guessed_letters.append(guess)
             if guess not in secret_word:
@@ -27,11 +35,14 @@ def play_game():
                 print(colorama.Fore.CYAN + "Oh no! The snowman is melting...")
             else:
                 print(colorama.Fore.CYAN + "That's a hit! Your guess prevented the snowman from melting!")
-        elif mistakes == 3:
-            break
 
 
 def get_valid_guess(guessed_letters):
+    """
+    Get a valid guess by the user.
+    :param guessed_letters: all letters guessed so far
+    :return: guessed letter
+    """
     while True:
         guess = input(colorama.Fore.BLUE + "\nGuess a letter: ").lower()
         if len(guess) != 1:
@@ -45,30 +56,34 @@ def get_valid_guess(guessed_letters):
 
 
 def display_game_state(mistakes, secret_word, guessed_letters):
+    """
+    Show current game state and word progress.
+    :param mistakes: number of incorrect guesses
+    :param secret_word: word to guess
+    :param guessed_letters: all letters guessed so far
+    :return: True if game is finished (win or lose), False otherwise
+    """
     print(colorama.Fore.CYAN + STAGES[mistakes])
     if mistakes == 3:
         print(colorama.Fore.MAGENTA + f"\nGame over, the snowman has completely melted!\n"
                                       f"The secret word was: {secret_word}\n")
-        play_again = does_user_want_to_play_again()
-        if play_again:
-            play_game()
-        else:
-            return
+        return True
     else:
         guessed_so_far = [char if char in guessed_letters else "_" for char in secret_word]
         if "_" in guessed_so_far:
             print(colorama.Fore.MAGENTA + "Word: ", "  ".join(guessed_so_far))
+            return False
         else:
             print(colorama.Fore.MAGENTA + f"\nCongratulations, you saved the snowman!\n"
                                           f"You found the secret word: {secret_word}\n")
-            play_again = does_user_want_to_play_again()
-            if play_again:
-                play_game()
-            else:
-                exit()
+            return True
 
 
 def does_user_want_to_play_again():
+    """
+    Ask user if he wants to play again.
+    :return: True or False depending on user input
+    """
     while True:
         play_again = input(colorama.Fore.YELLOW + "Do you want to play again? (Y/N) ").upper()
         if play_again not in ("Y", "YES", "N", "NO"):
@@ -80,8 +95,15 @@ def does_user_want_to_play_again():
 
 
 def main():
+    """
+    Start the game and handle replay loop.
+    """
     print(colorama.Style.BRIGHT + colorama.Fore.BLUE + "Welcome to Snowman Meltdown!")
-    play_game()
+    while True:
+        play_game()
+        play_again = does_user_want_to_play_again()
+        if not play_again:
+            break
 
 
 if __name__ == "__main__":
